3ae8b22a3b9d038b27a65a83ca35805a6a46bbe8
元玉鹏 <51024420+yuanyp8@users.noreply.github.com>
2026-07-06 08:47:26 +0800

    Declare IAM generated modules

---
+++ internal/server/modules.go
@@ -0,0 +1,25 @@
+package server
+
+import (
+	grantv1 "github.com/aisphereio/aisphere-iam/api/iam/grant/v1"
+	projectv1 "github.com/aisphereio/aisphere-iam/api/iam/project/v1"
+	resourcev1 "github.com/aisphereio/aisphere-iam/api/iam/resource/v1"
+	v1 "github.com/aisphereio/aisphere-iam/api/iam/v1"
+	"github.com/aisphereio/kernel/serverx"
+)
+
+func IAMModules() []serverx.ServiceModule {
+	return []serverx.ServiceModule{
+		v1.IAMAuthServiceKernelModule(),
+		v1.IAMDirectoryServiceKernelModule(),
+		v1.IAMIdentityAdminServiceKernelModule(),
+		v1.IAMPermissionServiceKernelModule(),
+		projectv1.ProjectServiceKernelModule(),
+		resourcev1.ResourceServiceKernelModule(),
+		grantv1.GrantServiceKernelModule(),
+	}
+}
+
+func IAMCatalog() serverx.ServiceCatalog {
+	return serverx.MustServiceCatalog(IAMModules()...)
+}
