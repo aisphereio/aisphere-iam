09a8537ad6f86a2a22d8659c6f4a9d531ea2dc2e
元玉鹏 <51024420+yuanyp8@users.noreply.github.com>
2026-07-06 09:11:11 +0800

    Register IAM gRPC through kernel

--- internal/server/grpc.go
+++ internal/server/grpc.go
@@ -10,6 +10,7 @@ import (
 	"github.com/aisphereio/aisphere-iam/internal/service"
 	"github.com/aisphereio/kernel/logx"
 	"github.com/aisphereio/kernel/metricsx"
+	"github.com/aisphereio/kernel/serverx"
 	kgrpc "github.com/aisphereio/kernel/transportx/grpc"
 )
 
@@ -32,12 +33,14 @@ func NewGRPCServer(c conf.ServerConfig, logCfg logx.Config, metricsCfg conf.Metr
 		opts = append(opts, kgrpc.Middleware(m...))
 	}
 	srv := kgrpc.NewServer(opts...)
-	v1.RegisterIAMAuthServiceServer(srv, authSvc)
-	v1.RegisterIAMDirectoryServiceServer(srv, dirSvc)
-	registerIdentityAdminRPC(srv, resources)
-	v1.RegisterIAMPermissionServiceServer(srv, permSvc)
-	projectv1.RegisterProjectServiceServer(srv, projectSvc)
-	resourcev1.RegisterResourceServiceServer(srv, resourceSvc)
-	grantv1.RegisterGrantServiceServer(srv, grantSvc)
+	if err := serverx.RegisterGRPCServices(srv, IAMBindings(resources, authSvc, dirSvc, permSvc, projectSvc, resourceSvc, grantSvc)...); err != nil {
+		v1.RegisterIAMAuthServiceServer(srv, authSvc)
+		v1.RegisterIAMDirectoryServiceServer(srv, dirSvc)
+		registerIdentityAdminRPC(srv, resources)
+		v1.RegisterIAMPermissionServiceServer(srv, permSvc)
+		projectv1.RegisterProjectServiceServer(srv, projectSvc)
+		resourcev1.RegisterResourceServiceServer(srv, resourceSvc)
+		grantv1.RegisterGrantServiceServer(srv, grantSvc)
+	}
 	return srv
 }
