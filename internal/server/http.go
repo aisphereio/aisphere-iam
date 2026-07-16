dc2eb723e555c39351f73bf60940c8110df81e0c
元玉鹏 <51024420+yuanyp8@users.noreply.github.com>
2026-07-16 10:57:18 +0800

    feat(openapi): register contract endpoints

--- internal/server/http.go
+++ internal/server/http.go
@@ -44,10 +44,11 @@ func NewHTTPServer(cfg conf.ServerConfig, logCfg logx.Config, metricsCfg conf.Me
 	if err := serverx.RegisterHTTPServices(srv, IAMBindings(resources, authSvc, dirSvc, groupSvc, permSvc, projectSvc, resourceSvc, grantSvc, accessQuerySvc)...); err != nil {
 		panic(err)
 	}
-v1.RegisterIAMAuthorizationAdminServiceHTTPServer(srv, authzAdminSvc)
-		registerProjectionBranches(srv, projections)
-		registerIdentityAuthZBranches(srv, resources)
-		registerDirectoryProjectionCompatibilityRoutes(srv, service.NewDirectoryProjectionOpsFromDeps(resources.Identity, resources.AuthzAdmin, resources.IdentityProjection))
+	v1.RegisterIAMAuthorizationAdminServiceHTTPServer(srv, authzAdminSvc)
+	registerProjectionBranches(srv, projections)
+	registerIdentityAuthZBranches(srv, resources)
+	registerDirectoryProjectionCompatibilityRoutes(srv, service.NewDirectoryProjectionOpsFromDeps(resources.Identity, resources.AuthzAdmin, resources.IdentityProjection))
+	registerOpenAPIContracts(srv)
 
 	srv.HandleFunc("/v1/iam/ui/login", func(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, safeUILoginReturnURL(r.URL.Query().Get("return_to")), http.StatusFound)
